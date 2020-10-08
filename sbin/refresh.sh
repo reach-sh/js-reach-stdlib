868b85123f028c26d8aaaf8657be9eb760dbb370
Dan Burton <danburton.email@gmail.com>
2020-10-08 16:20:03 -0400

    auto commit

--- sbin/refresh.sh
+++ sbin/refresh.sh
@@ -1,10 +1,14 @@
 #!/bin/sh
 set -ex
 
-docker run --entrypoint /bin/sh --volume $(pwd):/cwd reachsh/stdlib:latest \
+IMAGE=reachsh/stdlib:latest
+docker run --entrypoint /bin/sh --volume "$(pwd):/cwd" "$IMAGE" \
   -c 'cp /stdlib/*.mjs /stdlib/package.json /stdlib/*.d.ts /cwd/'
 
 rm tester.mjs ETH-test.mjs
 
 git diff package.json
 git checkout -- package.json
+
+HASH="$(docker run --entrypoint /bin/sh "$IMAGE" -c 'echo $REACH_GIT_HASH')"
+git commit -am "refresh -> reach-sh/reach-lang#$HASH"
