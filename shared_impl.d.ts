a03b0877c159feb505b157edad27f484009ed002
Dan Burton <danburton.email@gmail.com>
2020-10-15 16:53:37 -0400

    new file

---
+++ shared_impl.d.ts
@@ -0,0 +1,9 @@
+/**
+ * @description Create a getter/setter, where the getter defaults to memoizing a thunk
+ */
+export declare function replaceableThunk<T>(thunk: () => T): [() => T, (val: T) => void];
+/**
+ * @description Only perform side effects from thunk on the first call.
+ */
+export declare function memoizeThunk<T>(thunk: () => T): () => T;
+//# sourceMappingURL=shared_impl.d.ts.map
---
+++ shared_impl.mjs
@@ -0,0 +1,31 @@
+// Shared code with stuff that is not exported to the user
+/**
+ * @description Create a getter/setter, where the getter defaults to memoizing a thunk
+ */
+export function replaceableThunk(thunk) {
+  let called = false;
+  let res = null;
+
+  function get() {
+    if (!called) {
+      called = true;
+      res = thunk();
+    }
+    return res;
+  }
+
+  function set(val) {
+    if (called) {
+      throw Error(`Cannot re-set value once already set`);
+    }
+    res = val;
+    called = true;
+  }
+  return [get, set];
+}
+/**
+ * @description Only perform side effects from thunk on the first call.
+ */
+export function memoizeThunk(thunk) {
+  return replaceableThunk(thunk)[0];
+}
