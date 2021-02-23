b4cb3755ea247609794f80a95ea14ea2ce60e517
Dan Burton <danburton.email@gmail.com>
2021-02-22 20:01:05 -0500

    no more need to rm these

--- sbin/refresh.sh
+++ sbin/refresh.sh
@@ -7,7 +7,7 @@ IMAGE=reachsh/stdlib:latest
 docker run --entrypoint /bin/sh --volume "$(pwd):/cwd" "$IMAGE" \
   -c 'cp /stdlib/*.mjs /stdlib/package.json /stdlib/*.d.ts /cwd/'
 
-rm tester.mjs ETH-test.mjs
+# rm tester.mjs ETH-test.mjs
 
 git diff package.json
 git checkout -- package.json
