f8be02478975d636ab9dcda496f1760a6bc312b9
Dan Burton <danburton.email@gmail.com>
2021-07-06 11:56:06 -0400

    sanity check hash length

--- sbin/refresh.sh
+++ sbin/refresh.sh
@@ -1,23 +1,34 @@
 #!/bin/sh
 set -ex
 
-git diff --exit-code || (echo '\n\n\nGit repo is not clean; please commit first.' && exit 1)
+git diff --exit-code || (printf '\n\n\nGit repo is not clean; please commit first.' && exit 1)
 
 IMAGE=reachsh/stdlib:latest
-rm -f *.mjs *.d.ts
-rm -rf dist/
+HASH="$(docker run --entrypoint /bin/sh "$IMAGE" -c 'echo $REACH_GIT_HASH')"
+if [ "$(wc -c "$HASH")" = 9 ]; then
+  :
+else
+  set +x
+  echo
+  echo
+  echo "Hash length looks wrong: ${HASH}"
+  echo '(A trailing asterisk means the image was made from a dirty git repo)'
+  echo
+  exit 1
+fi
+
+rm -f ./*.mjs ./*.d.ts
+rm -rf ./dist/
 docker run --entrypoint /bin/sh --volume "$(pwd):/cwd" "$IMAGE" \
   -c 'cp /stdlib/*.mjs /stdlib/package.json /cwd/ && cp -r dist /cwd/dist && cp /stdlib/dist/types/* /cwd/'
   # -c 'cp /stdlib/*.mjs /stdlib/package.json /stdlib/*.d.ts /cwd/'
 
 
 # rm tester.mjs ETH-test.mjs
-rm version.mo{.d.ts,.mjs}
+rm ./version.mo.d.ts ./version.mo.mjs
 
 git diff package.json
 git checkout -- package.json
-
-HASH="$(docker run --entrypoint /bin/sh "$IMAGE" -c 'echo $REACH_GIT_HASH')"
 
 set +x
 echo
@@ -28,3 +39,4 @@ echo
 echo git add .
 echo git commit -m "'refresh -> reach-sh/reach-lang@$HASH'"
 echo '!!!!!!!!!!!!!!!!'
+echo
